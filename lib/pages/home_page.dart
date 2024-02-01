9ed74cdf4380bbf2dbb835ad6bbb346bbfb304f9
tarnishedrey <reyyasgod@gmail.com>
2024-02-01 13:08:34 +0700

    wrap widget w/ inkwell

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,21 +22,26 @@ class HomePage extends StatelessWidget {
             itemCount: items.length,
             itemBuilder: ((context, index) {
               final item = items[index];
-              return Card(
-                child: Container(
-                  margin: EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Expanded(child: Text(item.name)),
-                      Expanded(
-                          child: Text(item.price.toString(),
-                              textAlign: TextAlign.center)),
-                      Expanded(
-                          child: Text(
-                        item.description,
-                        textAlign: TextAlign.end,
-                      ))
-                    ],
+              return InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context, '/item');
+                },
+                child: Card(
+                  child: Container(
+                    margin: EdgeInsets.all(8),
+                    child: Row(
+                      children: [
+                        Expanded(child: Text(item.name)),
+                        Expanded(
+                            child: Text(item.price.toString(),
+                                textAlign: TextAlign.center)),
+                        Expanded(
+                            child: Text(
+                          item.description,
+                          textAlign: TextAlign.end,
+                        ))
+                      ],
+                    ),
                   ),
                 ),
               );
